bee68a8d688b0ee98573a3c57b3cad57014ad59e
Ekaterina Miheeva <karamilllka@gmail.com>
2021-10-13 00:14:46 +0600

    ver-2

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -2,15 +2,231 @@ package ru.netology.domain;
 
 public class Post {
     private int id;
-    private int publicationDate;
-    private String titlePictureImageUrl;
-    private String menuUrl;
-    private String mainText;
-    private String imageInTextUrl;
-    private int cunterLike;
-    private int cunterView;
-    private String buttonShare;
-    private String commentsInfo;
+    private int ovnerId;
+    private int fromId;
+    private int createdBy;
+    private int data;
+    private String text;
+    private int replyOwnerId;
+    private int replyPostId;
+    private CommetsInfo commetsInfo;
+    private Copyright copyright;
+    private ReportsInfo reportsInfo;
+    private LikesInfo likesInfo;
+    private ViewsInfo viewsInfo;
+    private String postType;
+    private PostSource postSource;
+    private Geo geo;
+    private int signerId;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private int markedAsAds;
+    private boolean isFavorite;
+    private int friendsOnly;
+    private Donut donut;
+    private int postponedId;
+
+    public int getOvnerId() {
+        return ovnerId;
+    }
+
+    public void setOvnerId(int ovnerId) {
+        this.ovnerId = ovnerId;
+    }
+
+    public int getFrom_id() {
+        return fromId;
+    }
+
+    public void setFromId(int fromId) {
+        this.fromId = fromId;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getReplyOwnerId() {
+        return replyOwnerId;
+    }
+
+    public void setReplyOwnerId(int replyOwnerId) {
+        this.replyOwnerId = replyOwnerId;
+    }
+
+    public int getReplyPostId() {
+        return replyPostId;
+    }
+
+    public void setReplyPostId(int replyPostId) {
+        this.replyPostId = replyPostId;
+    }
+
+    public CommetsInfo getCommetsInfo() {
+        return commetsInfo;
+    }
+
+    public void setCommetsInfo(CommetsInfo commetsInfo) {
+        this.commetsInfo = commetsInfo;
+    }
+
+    public Copyright getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(Copyright copyright) {
+        this.copyright = copyright;
+    }
+
+    public ReportsInfo getReportsInfo() {
+        return reportsInfo;
+    }
+
+    public void setReportsInfo(ReportsInfo reportsInfo) {
+        this.reportsInfo = reportsInfo;
+    }
+
+    public LikesInfo getLikesInfo() {
+        return likesInfo;
+    }
+
+    public void setLikesInfo(LikesInfo likesInfo) {
+        this.likesInfo = likesInfo;
+    }
+
+    public ViewsInfo getViewsInfo() {
+        return viewsInfo;
+    }
+
+    public void setViewsInfo(ViewsInfo viewsInfo) {
+        this.viewsInfo = viewsInfo;
+    }
+
+    public String getPostType() {
+        return postType;
+    }
+
+    public void setPostType(String postType) {
+        this.postType = postType;
+    }
+
+    public PostSource getPostSource() {
+        return postSource;
+    }
+
+    public void setPostSource(PostSource postSource) {
+        this.postSource = postSource;
+    }
+
+    public Geo getGeo() {
+        return geo;
+    }
+
+    public void setGeo(Geo geo) {
+        this.geo = geo;
+    }
+
+    public int getSignerId() {
+        return signerId;
+    }
+
+    public void setSignerId(int signerId) {
+        this.signerId = signerId;
+    }
+
+    public int getCanPin() {
+        return canPin;
+    }
+
+    public void setCanPin(int canPin) {
+        this.canPin = canPin;
+    }
+
+    public int getCanDelete() {
+        return canDelete;
+    }
+
+    public void setCanDelete(int canDelete) {
+        this.canDelete = canDelete;
+    }
+
+    public int getCanEdit() {
+        return canEdit;
+    }
+
+    public void setCanEdit(int canEdit) {
+        this.canEdit = canEdit;
+    }
+
+    public int getIsPinned() {
+        return isPinned;
+    }
+
+    public void setIs_pinned(int isPinned) {
+        this.isPinned = isPinned;
+    }
+
+    public int getMarkedAsAds() {
+        return markedAsAds;
+    }
+
+    public void setMarkedAsAds(int markedAsAds) {
+        this.markedAsAds = markedAsAds;
+    }
+
+    public boolean isIsFavorite() {
+        return isFavorite;
+    }
+
+    public void setIsFavorite(boolean isFavorite) {
+        this.isFavorite = isFavorite;
+    }
+
+    public Donut getDonut() {
+        return donut;
+    }
+
+    public void setDonut(Donut donut) {
+        this.donut = donut;
+    }
+
+    public int getPostponedId() {
+        return postponedId;
+    }
+
+    public void setPostponedId(int postponedId) {
+        this.postponedId = postponedId;
+    }
+
+    public int getFriendsOnly() {
+        return friendsOnly;
+    }
+
+    public void setFriendsOnly(int friendsOnly) {
+        this.friendsOnly = friendsOnly;
+    }
 
     public int getId() {
         return id;
@@ -18,77 +234,5 @@ public class Post {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public int getPublicationDate() {
-        return publicationDate;
-    }
-
-    public void setPublicationDate(int publicationDate) {
-        this.publicationDate = publicationDate;
-    }
-
-    public String getTitlePictureImageUrl() {
-        return titlePictureImageUrl;
-    }
-
-    public void setTitlePictureImageUrl(String titlePictureImageUrl) {
-        this.titlePictureImageUrl = titlePictureImageUrl;
-    }
-
-    public String getMenuUrl() {
-        return menuUrl;
-    }
-
-    public void setMenuUrl(String menuUrl) {
-        this.menuUrl = menuUrl;
-    }
-
-    public String getMainText() {
-        return mainText;
-    }
-
-    public void setMainText(String mainText) {
-        this.mainText = mainText;
-    }
-
-    public String getImageInTextUrl() {
-        return imageInTextUrl;
-    }
-
-    public void setImageInTextUrl(String imageInTextUrl) {
-        this.imageInTextUrl = imageInTextUrl;
-    }
-
-    public int getCunterLike() {
-        return cunterLike;
-    }
-
-    public void setCunterLike(int cunterLike) {
-        this.cunterLike = cunterLike;
-    }
-
-    public int getCunterView() {
-        return cunterView;
-    }
-
-    public void setCunterView(int cunterView) {
-        this.cunterView = cunterView;
-    }
-
-    public String getButtonShare() {
-        return buttonShare;
-    }
-
-    public void setButtonShare(String buttonShare) {
-        this.buttonShare = buttonShare;
-    }
-
-    public String getCommentsInfo() {
-        return commentsInfo;
-    }
-
-    public void setCommentsInfo(String commentsInfo) {
-        this.commentsInfo = commentsInfo;
     }
 }
